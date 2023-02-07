e7122984bc7dbfabe62aaf739bf31bd4cd473759
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-07 14:30:20 +0100

    ended without bonus

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!--stylesheet-->
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <!--Page Title-->
     <title>
@@ -72,6 +72,35 @@
 </head>
 
 <body>
+<div class="container my-4">
+    <h1 class="text-center my-5">HOTELS</h1>
+<table class="table pt-5">
+  <thead>
+    <tr>
+      <th scope="col"><h4>Name</h4></th>
+      <th scope="col"><h4>Description</h4></th>
+      <th scope="col"><h4>Parking</h4></th>
+      <th scope="col"><h4>Vote</h4></th>
+      <th scope="col"><h4>Distance to Center</h4></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($hotels as $hotel) : ?>
+    <tr>
+      <th scope="row"><i class="fa-solid fa-hotel"></i> <?= $hotel['name'] ?></th>
+      <td><i class="fa-regular fa-comment"></i> <?= $hotel['description'] ?></td>
+      <?php if ($hotel['parking']) : ?>
+        <td><i class="fa-solid fa-circle-check fa-xl"></i></td>
+        <?php else : ?>
+            <td><i class="fa-solid fa-circle-xmark fa-xl"></i></td>
+        <?php endif; ?>
+      <td><?= $hotel['vote'] ?> <i class="fa-solid fa-star"></i> </td>
+      <td><?= $hotel['distance_to_center'] ?> km</td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+</div>
 
 </body>
 
